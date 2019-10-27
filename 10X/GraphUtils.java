d18b9013ed2c8364e44a2af836e1f9beabd457a8
Matthew OTousa <mlotousa@gmail.com>
2019-10-26 19:36:22 -0700

    10X done i think

--- 10X/GraphUtils.java
+++ 10X/GraphUtils.java
@@ -3,64 +3,74 @@ import java.util.*;
 
 public abstract class GraphUtils {
 
-  static <V> Graph<V> emptyGraph() {
-      return new HiddenGraph<V>();
-  }
-
-  static <V> String dumpGraph(Graph<V> g) {
-    StringBuilder s = new StringBuilder();
-    s.append("Vertex count = " + g.vertexCount() + "\n");
-    for (V v : g.vertices()) {
-      s.append(v + ":");
-      for (V w : g.neighbors(v)) 
-        s.append(" " + w);
-      s.append("\n");
+    static <V> Graph<V> emptyGraph() {
+        return new HiddenGraph<V>();
     }
-    return s.toString();
-  }
 
-  static Graph<Integer> readIntegerGraph(String name) throws Exception {
-    Scanner in = new Scanner(new FileReader(name + ".ig"));
-    Graph<Integer> g = emptyGraph();
-    in.nextLine(); // read and ignore V
-    in.nextLine(); // read and ignore E
-    while (in.hasNext()) {
-      Integer v1 = in.nextInt();
-      Integer v2 = in.nextInt();
-      g.addVertex(v1); //might or might not already exist
-      g.addVertex(v2);
-      g.addEdge(v1,v2);
+    static <V> String dumpGraph(Graph<V> g) {
+        StringBuilder s = new StringBuilder();
+        s.append("Vertex count = " + g.vertexCount() + "\n");
+        for (V v : g.vertices()) {
+            s.append(v + ":");
+            for (V w : g.neighbors(v)) 
+                s.append(" " + w);
+            s.append("\n");
+        }
+        return s.toString();
     }
-    in.close();
-    return g;
-  }
 
-
-  static Graph<String> readStringGraph(String name, String sep) throws Exception {
-    Scanner in = new Scanner(new FileReader(name + ".sg"));
-    Graph<String> g = emptyGraph();
-    while (in.hasNext()) {
-      String[] a = in.nextLine().split(sep);
-      g.addVertex(a[0]); //might or might not already exist
-      for (int i = 1; i < a.length; i++) {
-        g.addVertex(a[i]);
-        g.addEdge(a[0],a[i]);
-      }
+    static Graph<Integer> readIntegerGraph(String name) throws Exception {
+        Scanner in = new Scanner(new FileReader(name + ".ig"));
+        Graph<Integer> g = emptyGraph();
+        in.nextLine(); // read and ignore V
+        in.nextLine(); // read and ignore E
+        while (in.hasNext()) {
+            Integer v1 = in.nextInt();
+            Integer v2 = in.nextInt();
+            g.addVertex(v1); //might or might not already exist
+            g.addVertex(v2);
+            g.addEdge(v1,v2);
+        }
+        in.close();
+        return g;
     }
-    in.close();
-    return g;
-  }
 
-  public static <V> void toDot(Graph <V> g, String gname) throws Exception {
-    java.io.PrintStream out = new java.io.PrintStream(gname + ".dot");
-    out.println("graph " + gname  + " {");
-    for (V v : g.vertices()) 
-      for (V w : g.neighbors(v)) 
-        out.println("\"" + v + "\" -- \"" + w + "\"");
-    out.println("}");
-    out.close();
-  }
-    
+
+    static Graph<String> readStringGraph(String name, String sep) throws Exception {
+        Scanner in = new Scanner(new FileReader(name + ".sg"));
+        Graph<String> g = emptyGraph();
+        while (in.hasNext()) {
+            String[] a = in.nextLine().split(sep);
+            g.addVertex(a[0]); //might or might not already exist
+            for (int i = 1; i < a.length; i++) {
+                g.addVertex(a[i]);
+                g.addEdge(a[0],a[i]);
+            }
+        }
+        in.close();
+        return g;
+    }
+
+    public static <V> void toDot(Graph <V> g, String gname) throws Exception {
+        java.io.PrintStream out = new java.io.PrintStream(gname + ".dot");
+        out.println("graph " + gname  + " {");
+        // TODO is this all dumb?
+        Set<Set<V>> edges = new HashSet<Set<V>>();
+        for (V v : g.vertices()) 
+            for (V w : g.neighbors(v)) {
+                HashSet<V> edge = new HashSet<V>();
+                edge.add(v);
+                edge.add(w);
+                edges.add(edge);
+            }
+        for (Set<V> sv : edges) {
+            Object[] a = sv.toArray(new Object[2]);
+            out.println("\"" + a[0] + "\" -- \"" + a[1] + "\"");
+        }
+        out.println("}");
+        out.close();
+    }
+
 
 }
 
